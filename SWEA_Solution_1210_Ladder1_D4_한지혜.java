e48f832a84bdeb3362c13d9a228c6f10bea9d6a4
Kylie Han <60127173+kylie-han@users.noreply.github.com>
2020-01-21 19:47:55 +0900

    Add files via upload

--- SWEA_Solution_1210_Ladder1_D4_한지혜.java
+++ SWEA_Solution_1210_Ladder1_D4_한지혜.java
@@ -5,14 +5,14 @@ public class SWEA_Solution_1210_Ladder1_D4_한지혜 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int TN = sc.nextInt();	//테스트케이스
-		int[][] arr = new int[10][10];	//배열의 크기 : 100,100
-		int x = 0,y = 9;
+		int[][] arr = new int[12][12];	//배열의 크기 : 100,100
+		int x = 10,y = 0;
 		//배열 생성
 		for (int i = 0; i < 1; i++) {//테스트 케이스 10개
-			for (int j = 0; j < arr.length; j++) {
-				for (int k = 0; k < arr[i].length; k++) {
-					arr[k][j] = sc.nextInt();
-					if(arr[k][j] == 2) x = j; 
+			for (int j = 1; j < arr.length-1; j++) {
+				for (int k = 1; k < arr[i].length-1; k++) {
+					arr[j][k] = sc.nextInt();
+					if(arr[j][k] == 2) y = j; 
 				}
 			}
 		}
@@ -20,15 +20,15 @@ public class SWEA_Solution_1210_Ladder1_D4_한지혜 {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(Arrays.toString(arr[i]));
 		}
-		System.out.println(x);//도착점의 위치->9
-		y--;//8
+		System.out.println(y);//도착점의 위치->10
+		x--;//9
 		for (int i = 0; i < 100 ; i++) {
-			if(arr[x-1][y] != 0) x--;
-			else if(arr[x+1][y] != 0) x++;
+			if(arr[x-1][y] == 1) x--;
+			else if(arr[x+1][y] == 1) x++;
 			else {
 				y--;
-				if (y<=0) {
-					System.out.println(y);
+				if (x<=0) {
+					System.out.println(x);
 					break;
 				}
 			}
@@ -37,3 +37,10 @@ public class SWEA_Solution_1210_Ladder1_D4_한지혜 {
 		System.out.println(y);
 	}//end of main
 }//end of class
+
+	
+
+/*
+1
+1 0 0 0 1 0 1 0 0 1 1 0 0 0 1 0 1 1 1 1 1 0 0 0 1 0 1 0 0 1 1 0 0 0 1 1 1 0 0 1 1 0 0 0 1 0 1 0 0 1 1 1 1 1 1 0 1 1 1 1 1 0 0 0 1 0 1 0 0 1 1 1 1 1 1 0 1 0 0 1 1 0 0 0 1 1 1 0 0 1 1 0 0 0 1 0 1 0 0 2
+*/
