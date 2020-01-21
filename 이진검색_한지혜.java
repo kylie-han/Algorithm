600d08dca2a08157f2c804d9531793541763bfe4
Kylie Han <60127173+kylie-han@users.noreply.github.com>
2020-01-22 01:26:38 +0900

    Add files via upload

--- 이진검색_한지혜.java
+++ 이진검색_한지혜.java
@@ -1,22 +1,41 @@
+  
 import java.util.Arrays;
 
 public class 이진검색_한지혜 {
 	public static void main(String[] args) {
-		int[] a = {4,9,11,23,2,19};
-		int n = 9;
+		int[] a = {4,9,11,23,2,19,10};
+		int find = 9;
+		int n = 0;
 		Arrays.sort(a);
+		n = a.length/2;
+		System.out.println(Arrays.toString(a));
 		for (int i = 0; i < a.length; i++) {
-			if (a[a.length/2] == n) {
+			int correct = 0;
+			int right = 0;
+			int left = 0;
+			if (a[n] == find) {
+				correct = 1;
+			}
+			else if (a[n] > find) {
+				left = 1;
+			}
+			else if (a[n] < find) {
+				right = 1;
+			}
+			System.out.println(correct+","+right+","+left);
+			if(correct == 1){
 				System.out.println("검색 성공");
-				continue;
+				break;
 			}
-			else if (a[a.length/2] > n) {
-			
+			else if (right == 1) {
+				n = n+n/2;
+				if(n/2 == 0) n++;
 			}
-			else if (a[a.length/2] < n) {
-				
+			else if (left == 1) {
+				n = n/2;
+				if(n/2 == 0) n--;
 			}
+			System.out.println(n);
 		}
-//		System.out.print(Arrays.toString(a));
 	}//end of main
 }//end of class
