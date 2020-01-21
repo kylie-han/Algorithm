8e5dcc13b1a49114ba9ccf80db4f1035e0439329
Kylie Han <60127173+kylie-han@users.noreply.github.com>
2020-01-22 01:42:45 +0900

    Add files via upload

--- 이진검색_한지혜.java
+++ 이진검색_한지혜.java
@@ -3,39 +3,25 @@ import java.util.Arrays;
 
 public class 이진검색_한지혜 {
 	public static void main(String[] args) {
-		int[] a = {4,9,11,23,2,19,10};
-		int find = 9;
+		int[] a = {4,9,11,23,2,19};
+		int find = 5;//찾고자 하는 값
 		int n = 0;
-		Arrays.sort(a);
+		Arrays.sort(a);//주어진 배열 정렬
 		n = a.length/2;
-		System.out.println(Arrays.toString(a));
+//		System.out.println(Arrays.toString(a));//배열 확인
 		for (int i = 0; i < a.length; i++) {
-			int correct = 0;
-			int right = 0;
-			int left = 0;
 			if (a[n] == find) {
-				correct = 1;
-			}
-			else if (a[n] > find) {
-				left = 1;
-			}
-			else if (a[n] < find) {
-				right = 1;
-			}
-			System.out.println(correct+","+right+","+left);
-			if(correct == 1){
 				System.out.println("검색 성공");
 				break;
 			}
-			else if (right == 1) {
-				n = n+n/2;
-				if(n/2 == 0) n++;
-			}
-			else if (left == 1) {
+			else if (a[n] > find) {
 				n = n/2;
 				if(n/2 == 0) n--;
 			}
-			System.out.println(n);
+			else if (a[n] < find) {
+				n = n+n/2;
+				if(n/2 == 0) n++;
+			}
 		}
 	}//end of main
 }//end of class
