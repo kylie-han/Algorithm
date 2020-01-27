9e59ea4556dc2cbc07491979107b4f38e4cc6c09
Kylie Han <hangji0124@gmail.com>
2020-01-28 01:13:20 +0900

    HW

---
+++ SWEA/SWEA_Solution_1859_백만장자프로젝트.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SWEA_Solution_1859_백만장자프로젝트 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int testCase = 1; testCase <= TC; testCase++) {
+			int inNum = sc.nextInt();
+			int[] a = new int[inNum+1];
+			for (int i = 0; i < a.length-1; i++) {
+				a[i] = sc.nextInt();
+			}//입력 배열에 저장
+			int max = a[inNum-1];
+			int sum = 0;
+//			for (int i = inNum-1; i >= 0; i--) {
+//				if(a[inNum]>=max) max = a[inNum];
+//				else sum += max-a[inNum];
+//			}//계산파트
+			int flag = 1;
+			for (int i = 0; i < a.length-1; i++) {
+				if(a[i]==a[i+1]) flag++;
+				else if(a[i]>a[i+1])flag = 1;
+				else if(a[i]<a[i+1]) {
+					sum += (a[i+1]-a[i])*flag;
+					flag++;
+				}
+			}//계산파트
+			System.out.printf("#%d %d\n",testCase,sum);
+		}//tc하나당 한번씩 실행
+	}//end of main
+}//end of class
--- com/ssafy/algo/DigitTest.java
+++ com/ssafy/algo/DigitTest.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class DigitTest {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr = ;
-		int x=1;
-		for (int i = 0; x==0 ; i++) {
-			x = sc.nextInt();
-			arr[i] = x;
+		int[] arr = new int[10];
+		while(true) {
+			int inData = sc.nextInt();
+			if(inData == 0) break;
+			arr[inData/10]++;
 		}
-	}
-}
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] != 0) {
+				System.out.println(i+" : "+arr[i]+"개");
+			}
+		}//end of for:출력단
+	}//end of main
+}//end of class
