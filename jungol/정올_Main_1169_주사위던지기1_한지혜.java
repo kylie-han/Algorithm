05d2f1e4e727b9b01a18ec05e00500e52416124d
Kylie Han <60127173+kylie-han@users.noreply.github.com>
2020-01-28 20:09:58 +0900

    Add files via upload

--- jungol/정올_Main_1169_주사위던지기1_한지혜.java
+++ jungol/정올_Main_1169_주사위던지기1_한지혜.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 정올_Main_1169_주사위던지기1_한지혜 {
@@ -6,13 +7,13 @@ public class 정올_Main_1169_주사위던지기1_한지혜 {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[] dice = {1,2,3,4,5,6};
-		int[] a = new int[n];
+		int[] arr = new int[n];
 		switch (m) {
 		case 1:
-			all(n);
+			m1(n,arr,0);
 			break;
 		case 2:
-			
+			m2(n,arr,0);
 			break;
 		case 3:
 			
@@ -22,16 +23,41 @@ public class 정올_Main_1169_주사위던지기1_한지혜 {
 			break;
 		}
 		
-	}
-	public static void all(int num) {
-		for (int i = 1; i <= num; i++) {
-			if (num <= 0) {
-				break;
-			} else {
-				System.out.print(i);
-				all(num-1);
-			} 
-			System.out.println();
+	}//end of main
+	public static void m1(int num, int[] a,int index) {
+		if (num<=0) {
+			for (int i = 0; i < a.length; i++) {
+				System.out.print(a[i]+" ");
+			}System.out.println();
+		} else {
+			for (int i = 1; i <= 6; i++) {
+				a[index] = i;
+				m1(num-1,a,index+1);
+			}
 		}
-	}
-}
+	}//end of m1
+	public static void m2(int num, int[] a,int index) {
+		if (num<=0) {
+			for (int i = 0; i < a.length; i++) {
+				System.out.print(a[i]+" ");
+			}System.out.println();
+		} else {
+			for (int i = index; i <= 6; i++) {
+				a[index] = i;
+				m2(num-1,a,index+1);
+			}
+		}
+	}//end of m2
+	public static void m3(int num, int[] a,int index) {
+		if (num<=0) {
+			for (int i = 0; i < a.length; i++) {
+				System.out.print(a[i]+" ");
+			}System.out.println();
+		} else {
+			for (int i = 1; i <= 6; i++) {
+				a[index] = i;
+				m3(num-1,a,index+1);
+			}
+		}
+	}//end of m2
+}//end of class
