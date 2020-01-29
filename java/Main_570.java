55114f3b5b74c0321ed89c819e94fb41ef5f3133
Kylie Han <60127173+kylie-han@users.noreply.github.com>
2020-01-29 15:34:56 +0900

    Add files via upload

---
+++ java/Main_570.java
@@ -0,0 +1,17 @@
+
+public class Main_570 {
+	public static void main(String[] args) {
+		int[][] arr = new int[7][7];
+		for (int i = 1; i < arr[i].length-1; i++) {
+			for (int j = 1; j < arr.length-1; j++) {
+				if(i==1 || j==1) {
+					arr[i][j] = 1;
+				}else {
+					arr[i][j] = arr[i-1][j] + arr[i][j-1];
+				}
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}//end of for
+	}//end of main
+}//end of class
