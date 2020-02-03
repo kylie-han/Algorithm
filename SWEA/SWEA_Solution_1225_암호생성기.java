6edbd3e10f61e67c231d11a46f00a49c01871674
Kylie Han <hangji0124@gmail.com>
2020-02-04 01:33:54 +0900

    0203HW

---
+++ SWEA/SWEA_Solution_1225_암호생성기.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SWEA_Solution_1225_암호생성기 {
+	public static int front = -1;
+	public static int rear = -1;
+	public static int[] arr = new int[8];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);//8자리 숫자 입력
+		for (int i = 1; i <= 10; i++) {
+			int testCase = sc.nextInt();
+			for (int j = 0; j < 8; j++) {
+				arr[j] = sc.nextInt();
+			}
+//			System.out.println(Arrays.toString(arr));
+			int index = 0;
+			boolean flag = true;
+			while (flag) {
+				for (int j = 1; j <= 5; j++) {
+					arr[index] -= j;
+//					System.out.println(index + " : " +arr[index]);
+					if(arr[index]<=0) {
+						arr[index] = 0;
+						flag = false;
+						break;
+					}
+					if (index >= arr.length-1) index = 0;
+					else index++;
+				}
+			}
+			System.out.print("#" + testCase);
+			for (int j = 0; j < arr.length; j++) {
+				++index;
+				if (index > arr.length-1) index = 0;
+				System.out.print(" "+arr[index]);
+			}
+			System.out.println();
+		}
+	}//end of main
+}//end of class
--- java-algo/Z24_연습문제.java
+++ java-algo/Z24_연습문제.java
@@ -1,26 +1,38 @@
 //0203_과제 : 마이쭈, 1225 - SW문제해결 기본 7일차 - 암호생성기
 public class Z24_연습문제 {
-	public static int[] arr = new int[20];
+	public static int[] arr = new int[30];
 	public static int front = -1;
 	public static int rear = -1;
+
 	public static void main(String[] args) {
 		int candy = 20;
 		int[] many = new int[10];
-		
-		//줄을 선다.
-		
-		for (int i = 0; i < many.length; i++) {
-			many[1]++;
-			enQueue(1);
+		int newPeople = 1;
+		// 줄을 선다.
+		enQueue(1);
+//		System.out.println("1번이 줄을 선다.");
+		while (candy > 0) {
 			int x = deQueue();
+			many[x]++;
+			candy -= many[x];
+//			System.out.println("  "+x+"번이 "+many[x]+"개의 마이쮸를 받는다.");
+//			System.out.println("마이쮸"+candy+"개 남음");
+			if (candy<=0) {
+				 System.out.println("마지막 마이쮸는 "+ x + "가 가져감");
+				 break;
+			}
 			enQueue(x);
-			enQueue(x + 1);
+//			System.out.println(x+"번이 다시 줄을 선다.");
+			newPeople++;
+			enQueue(newPeople);
+//			System.out.println("새로 "+ newPeople + "번이 들어와 줄을 선다.");
 		}
-		
-		//마이쮸를 받는다. -> 다른 친구가 줄을 선다.
-		
+
+		// 마이쮸를 받는다. -> 다른 친구가 줄을 선다.
+
 	}
-	private static int deQueue() {
+
+	public static int deQueue() {
 		// TODO Auto-generated method stub
 		front++;
 		return arr[front];
