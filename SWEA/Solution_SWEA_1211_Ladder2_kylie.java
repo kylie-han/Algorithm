7e297b4c883e48faa97a2d9a8fd3e40c93a2514f
Jihye Han <hangji0124@gmail.com>
2021-01-18 16:26:11 +0900

    S/W 문제해결 기본 2일차문제(문제내용에 오류가 있어 푸는데 오래걸림)

--- SWEA/Solution_SWEA_1211_Ladder2_kylie.java
+++ SWEA/Solution_SWEA_1211_Ladder2_kylie.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 // 문제를 잘못 이해함.
 // 문제의 요점은 사다리를 타는데 이동한 횟수가 가장 짧은 시작점을 찾는 것.
@@ -9,55 +10,73 @@ public class Solution_SWEA_1211_Ladder2_kylie {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for (int i = 0; i < 10; i++) {
-			visited = new boolean[100][100];
 			int ans = 0;
-			// input tc
+			// 테스트케이스 번호
 			sc.nextInt();
-			// 입력되는 값 전체
+			// 맵전체
+			int startcnt = 0;
 			for (int j = 0; j < map.length; j++) {
 				for (int k = 0; k < map.length; k++) {
 					map[j][k] = sc.nextInt();
-					// 입력값이 2일 경우 폭탄자리
-					if(j == map.length-1 && map[j][k] == 2) {
-						bombX = k;
-						bombY = j;
+					if(j == 0 && map[j][k] == 1) {
+						// 시작점의 개수
+						startcnt++;
 					}
 				}
 			}
-			// 한칸 올라가서
-			bombY--;
-			// 가장위칸까지 반복
-			while(bombY > 0) {
-				System.out.println(bombY +" / "+bombX);
-				visited[bombY][bombX] = true;
-				// 왼쪽에 방문하지 않은 1이 있으면
-				if(bombX-1>=0 && !visited[bombY][bombX-1] && map[bombY][bombX-1] == 1) {
-					bombX--;
-					visited[bombY][bombX] = true;
-					// 위에 1이 생길때까지 X--
-					while(map[bombY-1][bombX] != 1) {
-						bombX--;
-						visited[bombY][bombX] = true;
-					}
-				}
-				// 오른쪽에 방문하지 않은 1이 있으면
-				else if(bombX+1<100 && !visited[bombY][bombX+1] && map[bombY][bombX+1] == 1) {
-					bombX++;
-					visited[bombY][bombX] = true;
-					// 위에 1이 생길때까지 X++
-					while(map[bombY-1][bombX] != 1) {
-						bombX++;
-						visited[bombY][bombX] = true;
-					}
-				}
-				// 양쪽에 방문하지 않은 1이 없으면
-				else {
-					bombY--;
+//			System.out.println(startcnt);
+			int[] start = new int[startcnt];
+			int[] end = new int[startcnt];
+			int idx = 0;
+			for(int j = 0; j < map.length; j++) {
+				if(map[0][j] == 1) {
+					start[idx++] = j;
 				}
 			}
-			ans = bombX;
-			
-			
+//			System.out.println(Arrays.toString(start));
+			int min = Integer.MAX_VALUE;
+			int cnt = 0;
+			// 모든 시작점을 끝까지 진행하고 횟수를 카운트하여 end배열에 저장한다.
+			for (int j = 0; j < startcnt; j++) {
+				visited = new boolean[100][100];
+				int r = 0, c = 0;
+				c = start[j];
+				cnt = 0;
+				visited[r][c] = true;
+				while(r < 99) {
+//					System.out.println(r +" / "+ c);
+					// 왼쪽에 갈 수 있는 사다리가 있으면 내려가는 길이 있을때까지 직진
+					if(c>0 && map[r][c-1] == 1 && !visited[r][c-1]) {
+						c--;
+						cnt++;
+						visited[r][c] = true;
+						while(map[r+1][c] != 1) {
+							c--;
+							cnt++;
+							visited[r][c] = true;
+						}
+					}
+					// 오른쪽에 갈 수 있는 사다리가 있으면 내려가는 길이 있을때까지 직진
+					else if(c<99 && map[r][c+1] == 1 && !visited[r][c+1]) {
+						c++;
+						cnt++;
+						visited[r][c] = true;
+						while(map[r+1][c] != 1) {
+							c++;
+							cnt++;
+							visited[r][c] = true;
+						}
+					}
+					r++;
+					cnt++;
+					visited[r][c] = true;
+				}
+				// 가장작은 수가 ans
+				if(min >= cnt) {
+					min = cnt;
+					ans = start[j];
+				}
+			}
 			System.out.println("#"+(i+1)+" "+ans);
 		}
 	}
