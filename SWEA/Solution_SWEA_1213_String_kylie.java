295fb8606c672499edf825f1cee57c33befb025e
Jihye Han <hangji0124@gmail.com>
2021-01-19 23:40:12 +0900

    0119 solving

---
+++ SWEA/Solution_SWEA_1213_String_kylie.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution_SWEA_1213_String_kylie {
+	static int T = 10;
+	static int ans = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < T; i++) {
+			int t = sc.nextInt();
+			String part = sc.next();
+			String all = sc.next();
+			
+			for(int j = 0; j < all.length(); j++) {
+				
+			}
+			
+			System.out.println("#"+ t+ " "+ ans);
+		}//tc
+	}//main
+}
