fa1972e24dbff644c78aa3324f233ee0c12d8311
Jihye Han <hangji0124@gmail.com>
2021-01-28 16:47:08 +0900

    easy

---
+++ boj/boj_2741_N찍기_Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class boj_2741_NÂï±â_Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+	}
+}
