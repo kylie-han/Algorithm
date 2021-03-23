2090584de8b4ac3bca855a423aef08d21c266c78
Jihye Han <hangji0124@gmail.com>
2021-03-23 11:32:21 +0900

    aps solving

--- boj/Boj_14501_Main.java
+++ boj/Boj_14501_Main.java
@@ -13,7 +13,23 @@ public class Boj_14501_Main {
         for (int i = 0; i < TP.length; i++) {
             TP[i][0] = sc.nextInt();
             TP[i][1] = sc.nextInt();
-            System.out.println(Arrays.toString(TP));
+            // System.out.println(Arrays.toString(TP[i]));
         }
+        ans = 0;
+        for (int i = dp.length-1; i >= 0; i--) {
+            if(TP[i][0] + i >N){
+                dp[i] = 0;
+            }else if(TP[i][0] + i == N){
+                dp[i] = TP[i][1];
+            }else{
+                dp[i] = TP[i][1]+dp[i+TP[i][0]];
+            }
+            if(dp[i] > ans){
+                ans = dp[i];
+            }else{
+                dp[i] = ans;
+            }
+        }
+        System.out.println(ans);
     }
 }
